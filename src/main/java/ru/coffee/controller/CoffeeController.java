884ae2c41a3f6a78d3daf2fc65f9ca4cb22674c7
vital_63_ <vital_63_@mail.ru>
2017-09-05 13:32:30 +0300

    Refactoring: Extract CoffeeControllerManager.

--- src/main/java/ru/coffee/controller/CoffeeController.java
+++ src/main/java/ru/coffee/controller/CoffeeController.java
@@ -1,110 +1,47 @@
 package ru.coffee.controller;
 
 import java.io.IOException;
-import java.sql.SQLException;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import ru.coffee.service.CoffeeService;
-import ru.coffee.validator.Validator;
 
 public class CoffeeController extends HttpServlet {
 
-    private CoffeeService coffeeService = new CoffeeService();
+    CoffeeControllerManager controllerManager = new CoffeeControllerManager();
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getServletPath();
         switch (action) {
             case "/Delivery":
-                delivery(request, response);
+                controllerManager.delivery(request, response);
                 break;
             case "/CreateOrder":
-                createOrder(request, response);
+                controllerManager.createOrder(request, response);
                 break;
             case "/Confirmation":
-                confirm(request, response);
+                controllerManager.confirm(request, response);
                 break;
             default:
-                listCoffee(request, response);
+                controllerManager.listCoffee(request, response);
         }
     }
     
-    private void listCoffee(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try {
-            coffeeService.listCoffee(request);
-            forwardToView(request, response, "/CoffeeList.jsp");
-        } catch (SQLException ex) {
-            throw new ServletException(ex);
-        }
-    }
-    
-    private void delivery(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if("POST".equals(request.getMethod())){
-            if(!Validator.validateListCoffee(request)){
-                response.sendRedirect(request.getHeader("Referer"));
-                return;
-            }
-            coffeeService.prepareOrder(request);
-        }
-        forwardToView(request, response, "/Delivery.jsp");
-    }    
-    
-    private void createOrder(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (!Validator.validateAddress(request)) {
-            response.sendRedirect(request.getHeader("Referer"));
-            return;
-        }
-        coffeeService.createOrder(request);
-        response.sendRedirect("Confirmation");
-    }
-    
-    private void confirm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        forwardToView(request, response, "/Confirmation.jsp");
-    }
-    
-    private static void forwardToView(HttpServletRequest request, HttpServletResponse response, String path) throws ServletException, IOException{
-        RequestDispatcher rd = request.getRequestDispatcher(path);
-        rd.forward(request, response);   
-    }
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
---
+++ src/main/java/ru/coffee/controller/CoffeeControllerManager.java
@@ -0,0 +1,53 @@
+package ru.coffee.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import ru.coffee.service.CoffeeService;
+import ru.coffee.validator.Validator;
+
+public class CoffeeControllerManager {
+
+    private final CoffeeService coffeeService = new CoffeeService();
+    
+    public void listCoffee(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            coffeeService.listCoffee(request);
+            forwardToView(request, response, "/CoffeeList.jsp");
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    public void delivery(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if ("POST".equals(request.getMethod())) {
+            if (!Validator.validateListCoffee(request)) {
+                response.sendRedirect(request.getHeader("Referer"));
+                return;
+            }
+            coffeeService.prepareOrder(request);
+        }
+        forwardToView(request, response, "/Delivery.jsp");
+    }
+
+    public void createOrder(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (!Validator.validateAddress(request)) {
+            response.sendRedirect(request.getHeader("Referer"));
+            return;
+        }
+        coffeeService.createOrder(request);
+        response.sendRedirect("Confirmation");
+    }
+
+    public void confirm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        forwardToView(request, response, "/Confirmation.jsp");
+    }
+
+    private static void forwardToView(HttpServletRequest request, HttpServletResponse response, String path) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher(path);
+        rd.forward(request, response);
+    }
+}
